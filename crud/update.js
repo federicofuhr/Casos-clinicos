2d5236f583bb733e7ad203c32335f46975e2a7cc
juanbarthes <jbarthes@alumnos.exa.unicen.edu.ar>
2022-11-25 20:48:21 -0300

    se acomodo el formato de update.js y se removieron console.log() innecesarios

--- crud/update.js
+++ crud/update.js
@@ -3,17 +3,25 @@ const Medic = require("../models/Medic");
 const ClinicCase = require("../models/ClinicCase");
 
 async function updatePatient() {
-  const patient = await Patient.update({ dni: 37257130 }, {
+  const patient = await Patient.update({
+    dni: 37257130
+  }, {
     phone: '2266445564'
   });
-  const updatedPatient = await Patient.findOne({ dni: 37257130 })
+  const updatedPatient = await Patient.findOne({
+    dni: 37257130
+  })
   console.log("Paciente luego de actualizar su numero de telefono");
   console.log(updatedPatient)
 
-  const clinicCase = await ClinicCase.find({"patient.dni": 37257130})
-  
+  const clinicCase = await ClinicCase.find({
+    "patient.dni": 37257130
+  })
+
   clinicCase.forEach(async (c) => {
-    await ClinicCase.update({caseNumber: c.caseNumber}, {
+    await ClinicCase.update({
+      caseNumber: c.caseNumber
+    }, {
       patient: updatedPatient
     })
   });
@@ -21,28 +29,36 @@ async function updatePatient() {
 
 
 async function updateMedic() {
-  const medic = await Medic.update({ registrationNumber: 123456 }, {
+  const medic = await Medic.update({
+    registrationNumber: 123456
+  }, {
     address: 'Avenida España 600'
   });
-  const updatedMedic = await Medic.findOne({ registrationNumber: 123456 })
+  const updatedMedic = await Medic.findOne({
+    registrationNumber: 123456
+  })
   console.log("Medico luego de actualizar su direccion");
   console.log(updatedMedic)
 
 
-  const clinicCase = await ClinicCase.find({ "medics.registrationNumber": 123456 })
-  console.log("clinicCase impreso");
-  console.log(clinicCase);
+  const clinicCase = await ClinicCase.find({
+    "medics.registrationNumber": 123456
+  })
 
   clinicCase.forEach(async (c) => {
-    console.log("Medico luego de actualizar su direccion (dentro de foreach)");
-    console.log(updatedMedic)
-    await ClinicCase.update({ caseNumber: c.caseNumber }, {
+    await ClinicCase.update({
+      caseNumber: c.caseNumber
+    }, {
       $pull: {
-        medics: { registrationNumber: updatedMedic.registrationNumber }
+        medics: {
+          registrationNumber: updatedMedic.registrationNumber
+        }
       }
     })
 
-    await ClinicCase.update({ caseNumber: c.caseNumber }, {
+    await ClinicCase.update({
+      caseNumber: c.caseNumber
+    }, {
       $push: {
         medics: updatedMedic
       }
