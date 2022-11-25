120e72e9628f463ff9323c715ae3e9bf8e171dfe
juanbarthes <jbarthes@alumnos.exa.unicen.edu.ar>
2022-11-25 01:46:26 -0300

    actualizado el update.js

--- crud/update.js
+++ crud/update.js
@@ -1,13 +1,45 @@
 require("../connection");
 
-const User = require("../models/User");
+const Patient = require("../models/Patient");
+const Medic = require("../models/Medic");
+const ClinicCase = require("../models/ClinicCase");
 
-async function updateUsers() {
-  const user = await User.update({username: 'jose'},{
-    password: 'contraseniaSegura'
+async function updatePatient() {
+  const patient = await Patient.update({dni: 37257130},{
+    phone: '2266445564'
   });
-
-  console.log(user)
+  const updatedPatient = await Patient.findOne({dni: 37257130})
+  console.log("Paciente luego de actualizar su numero de telefono");
+  console.log(updatedPatient)
 }
 
-updateUsers()
+
+async function updateMedic() {
+  const medic = await Medic.update({registrationNumber: 123456},{
+    address: 'Avenida España 600'
+  });
+  const updatedMedic = await Medic.findOne({registrationNumber: 123456})
+  console.log("Medico luego de actualizar su direccion");
+  console.log(updatedMedic)
+
+  const clinicCase = await ClinicCase.find({"medics.registrationNumber": 123456})
+  console.log("clinicCase impreso");
+  console.log(clinicCase);
+  
+  clinicCase.forEach(async (c, updatedMedic) => {
+    await ClinicCase.update({caseNumber: c.caseNumber}, {
+      $pull: {
+        medics: {registrationNumber: updatedMedic.registrationNumber}
+      }
+    })
+
+    await ClinicCase.update({caseNumber: c.caseNumber}, {
+      $push: {
+        medics: {updatedMedic}
+      }
+    })
+  });
+}
+
+updatePatient()
+updateMedic()
